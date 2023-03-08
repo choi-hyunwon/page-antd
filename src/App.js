4c380fe8f77f26cd89af7b97b0a99da881075b66
choihyunwon <danboys@domfam.com>
2023-03-08 15:06:30 +0900

    layout check

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Layout, Menu, theme } from 'antd';
 import './index.css';
-
 import {Route, Routes} from "react-router-dom";
 import RouteList from "./app/router";
 
+const { Header, Content, Footer, Sider } = Layout;
+
+
+
 const App = () => {
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
     return (
-        <>
-            <Header/>
-            <div >
-                <Routes>
-                    {
-                        RouteList.map((item, index) => (
-                            <Route key={index} {...item} />
-                        ))
-                    }
-                </Routes>
-            </div>
-            <Footer/>
-        </>
+        <Layout>
+            <Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                onBreakpoint={(broken) => {
+                    console.log(broken);
+                }}
+                onCollapse={(collapsed, type) => {
+                    console.log(collapsed, type);
+                }}
+            >
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['4']}
+                    items={[UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
+                        (icon, index) => ({
+                            key: String(index + 1),
+                            icon: React.createElement(icon),
+                            label: `nav ${index + 1}`,
+                        }),
+                    )}
+                />
+            </Sider>
+            <Layout>
+                <Header style={{ padding: 0, background: colorBgContainer }} />
+                <Content style={{ margin: '24px 16px 0' }}>
+                    <Routes>
+                        {
+                            RouteList.map((item, index) => (
+                                <Route key={index} {...item} />
+                            ))
+                        }
+                    </Routes>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+            </Layout>
+        </Layout>
     );
 };
 
